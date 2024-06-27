5d75dfbcbaa53cc2b3c128529bf588fe43a9d280
zahrafeisal <zahrafeisal3@gmail.com>
2024-06-27 07:50:54 +0300

    added event listener

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+const input = document.getElementById("button");
+
+function clickAlert() {
+    alert("I've been clicked!");
+}
+
+input.addEventListener("click", clickAlert);
+//addEventListener("click", clickAlert);
 }
